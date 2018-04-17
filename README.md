0d4fc1a46499f82e881f533a8b459d25185a940f
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-04-17 16:34:48 -0400

    notebook

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Scan Statistic for Kataegis Identification
+
+```
+conda env create -f environment.yml
+# OR
+conda env update -f environment.yml
+
+source activate scan_statistic
+jupyter notebook
+
+source deactivate
+```
--- run.py
+++ run.py
@@ -8,16 +8,18 @@ parser = argparse.ArgumentParser(description='Scan statistic to identify kataegi
 
 args = parser.parse_args()
 
-input_dir = 'data'
-input_filenames = list(filter(lambda x: x.endswith(".tsv"), os.listdir(input_dir)))
-df = None
-for input_filename in input_filenames:
-    file_df = pd.read_csv(os.path.join(input_dir, input_filename), sep='\t')
-    if df is None:
-        df = file_df
-    else:
-        df = df.append(file_df)
+def load_df(input_dir = 'data')
+  input_filenames = list(filter(lambda x: x.endswith(".tsv"), os.listdir(input_dir)))
+  df = None
+  for input_filename in input_filenames:
+      file_df = pd.read_csv(os.path.join(input_dir, input_filename), sep='\t')
+      if df is None:
+          df = file_df
+      else:
+          df = df.append(file_df)
+  return df
 
+df = load_df()
 bss = BernoulliScanStatistic(df)
 bss.monte_carlo()
 
